fa6c1e5f7a077ee21d71330f35542b8f5fa3ac04
Szymon-PC <wachim17@gmail.com>
2023-01-15 19:02:14 +0100

    Dodane klasy skojarzone z DB

---
+++ Nurses_Scheduler/Classes/Department.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nurses_Scheduler.Classes
+{
+    [Table("Department")]
+    public class Department
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        
+        public string DepartmentName { get; set; }
+
+        public string DepartmentShortName { get; set; }
+    }
+}
---
+++ Nurses_Scheduler/Classes/Employee.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nurses_Scheduler.Classes
+{
+    [Table("Employee")]
+    internal class Employee
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public string Occupation { get; set; }  
+
+        [Ignore]
+        public string FullName 
+        { 
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
