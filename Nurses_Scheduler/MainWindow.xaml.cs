91c91d555c18cee54a530e6ec7051f3b0237ef79
Szymon-PC <wachim17@gmail.com>
2023-02-16 22:47:41 +0100

    little updates

--- Nurses_Scheduler/MainWindow.xaml.cs
+++ Nurses_Scheduler/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using Python;
 
 namespace Nurses_Scheduler
 {
